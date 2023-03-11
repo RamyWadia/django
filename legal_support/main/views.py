5feb51a2828c58d1eb2bd30aff3b3a2b9cb590ff
Ramy Atalla <ramywadia@gmail.com>
2023-03-11 18:08:11 +0200

    add user registration and login

--- legal_support/main/views.py
+++ legal_support/main/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterFrom
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 def home(req):
     return render(req, "main/home.html")
+
+def sign_up(req):
+    if req.method == "POST":
+        form = RegisterFrom(req.POST)
+        if form.is_valid():
+            user = form.save()
+            login(req, user)
+            return redirect("/home")
+    else:
+        form = RegisterFrom()
+    return render(req, "registration/sign_up.html", {"form": form}) 
